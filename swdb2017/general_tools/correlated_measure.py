28306541c9eb53d95672a5c5f7120e411079d90d
Sean Jewell <jewellsean@gmail.com>
2017-09-01 15:50:18 -0700

    index out of corr

--- swdb2017/general_tools/correlated_measure.py
+++ swdb2017/general_tools/correlated_measure.py
@@ -12,7 +12,7 @@ def permute(orig_mtx):
 	return(X_permuted)
 
 def matrix_similarity(model_mtx, mtx):
-	return(np.corrcoef(model_mtx.flatten(), mtx.flatten()))
+	return(np.corrcoef(model_mtx.flatten(), mtx.flatten())[1][0])
 	
 
 def calculate_sampling_dist(model_mtx, emp_mtx, num_samples):
